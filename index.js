d9b11437c7146ca08b8ba5022536ffa650106596
Cheung Tsz Yin <cheungkevinviola909@gmail.com>
2021-01-21 17:11:14 +0800

    added db config

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const app = express();
+
+require('dotenv').config({path:'../.env'});
+
 let port = process.env.PORT || 5000;
 
-app.get("/", (req, res)=> {
-    res.send("Hello World");
+mongoose.connect( "mongodb+srv://devadmin:elT16PlPKurjwqbv@cluster0.eu3sn.mongodb.net/Cluster0?retryWrites=true&w=majority", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+	/*,
+	useFindAndModify: false,
+	useCreateIndex: true,
+	*/
+	}, () => {
+		let connectionStation = "";
+		if (mongoose.connection.readyState === 1) connectionStation = "DB connection: Successful";
+		else connectionStation = "DB connection: Failure";
+		console.log(connectionStation);
+	}
+);
+
+app.get("/", (req, res) => {
+	res.send("Hello World");
 });
 
 app.listen(port, () => {
-    console.log(`Running on port: ${port}`);
+	console.log(`Running on port: ${port}`);
 });
