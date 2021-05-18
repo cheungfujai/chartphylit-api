894e6bcca52b5bb94a6a7cbe970aa9ae61e6efa4
Johnson Ma <jabber484@gmail.com>
2021-05-18 08:00:30 +0000

    profile change

--- models/profile.js
+++ models/profile.js
@@ -65,7 +65,7 @@ const ProfileSchema = new mongoose.Schema({
         required: true
     },
     durationDaysActivity: {
-        type: String,
+        type: [String],
         required: false
     },
     sportLevel: {
